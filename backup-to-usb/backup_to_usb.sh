adfb3ab688657acbea83530cd4aa1b21b19907b3
Peter Ryan <me@peter-ryan.co.uk>
2022-10-02 16:03:14 +0100

    Update/refactor code

--- backup-to-usb/backup_to_usb.sh
+++ backup-to-usb/backup_to_usb.sh
@@ -1,59 +1,75 @@
 #!/bin/bash
+#
+# Use rsync to copy files from $HOME to a user selected media (e.g. USB flash)
 
-# Might be worth emailing any errors... as the user likely won't report them... and/or use logger?
+# TODO: Log/email errors? It is likely the user won't report them.
 
 #source=$HOME/Documents
 source=$HOME/
 destination='/Backups/'
 
+#rsync_zenity_awk='/usr/local/bin/backup_to_usb_rsync.awk'
+rsync_zenity_awk='backup_to_usb_rsync.awk'
+#rsync_filter='.rsync-backup-filter'
+rsync_filter='.rsync-usbflash-filter'
 
-# Infinite loop
-until [ 1 -eq 0  ]; do
+#######################################
+# Output error message to stderr
+#######################################
+err() {
+	echo -e "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+
+# Request device mount-point from user
+until [[ 1 -eq 0 ]]; do
 	mount=$( \
-		sed -n 's/^[^ ]\+[[:space:]]\(\/media\/[^/ ]\+\)[[:space:]].*$/\1/p' /proc/mounts | \
-		sed 's/\\040/ /' | \
-		zenity --list --column "Available devices" \
+		sed -n 's/^[^ ]\+[[:space:]]\(\/media\/[^/ ]\+\)[[:space:]].*$/\1/p' /proc/mounts \
+		| sed 's/\\040/ /' \
+		| zenity --list --column "Available devices" \
 			--title "Choose sync device" \
 			--text "Select the device to synchronise to:" \
 	)
 	# If the user pressed Cancel, we exit the script
-	if [ $? -ne 0 ]; then
-		printf "Operation canceled by user\n" >&2
+	if [[ $? -ne 0 ]]; then
+		err "Operation canceled by user"
 		exit 1
 	fi
 	# If $mount contains a string, we can exit the loop
-	test "$mount" && break
+	if [[ -n "${mount}" ]]; then
+		break
+	fi
 done
 
 
-path=$mount$destination
-
-
-if [ ! -d "$path" ]; then
-	printf "Backup path not a directory [%s]\n" "$path" >&2
+# Check destination path is a valid dir
+path=${mount}${destination}
+if [[ ! -d "$path" ]]; then
+	err "Backup path not a directory [${path}]"
 	zenity --error \
 		--title "Error: No backup directory" \
-		--text "Backup path not a directory:\n$path"
-	exit 2
+		--text "Backup path not a directory:\n${path}"
+	exit 1
 fi
 
 
 
-# The rsync/zenity progress stuff was borrowed from this site:
-# http://blog.crowdway.com/2008/12/24/zenity-rsync-and-awk/
+# The rsync/zenity progress code was borrowed from this site:
+# https://davidverhasselt.com/zenity-rsync-and-awk/
 
+# Use rsync to copy files from source to destination; log and report any errors
 errmsg=$(( \
-	rsync -av --progress --delete --filter='dir-merge .rsync-backup-filter' "$source" "$path" | \
-	awk -f /usr/local/bin/backup_to_usb_rsync.awk | \
-	zenity --progress --title "Backing up to $mount" \
+	rsync -av --progress --delete --filter='dir-merge ${rsync_filter}' "$source" "$path" \
+	| awk -f ${rsync_zenity_awk} \
+	| zenity --progress --title "Backing up to ${mount}" \
 		--text="Please wait..." --percentage=0 --auto-kill \
 	) 2>&1)
 
-if [ -n "$errmsg" ]; then
-	printf "Errors occured during rsync:\n%s\n" "$errmsg" >&2
+if [[ -n "$errmsg" ]]; then
+	err "Errors occured during rsync:\n${errmsg}"
 	zenity --error \
 		--title "Error: rsync problem" \
-		--text "Errors occured during rsync:\n\n$errmsg"
-	exit 3
+		--text "Errors occured during rsync:\n\n${errmsg}"
+	exit 1
 fi
 
